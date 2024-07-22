783ad28e4f6b84e31d4dc3a07c8a427ed2d6c73e
Obaida Almelhem <111151192+Obaida00@users.noreply.github.com>
2024-07-23 01:22:07 +0300

    added descriptions to the data directory and to the products csv, also the name is being taken from the description not the file name

--- Description_Fill.py
+++ Description_Fill.py
@@ -1,20 +1,22 @@
 import os
 import time
 
-DIR_PATH = 'f:/REPO/Almousoaa-Product-Automations/'
+DIR_PATH = "f:/REPO/Almousoaa-Product-Automations/"
 
-def find(name, path):
-    name = name +'.txt'
+
+def find(name: str, path):
+    name = name + ".txt"
     for root, dirs, files in os.walk(path):
         if name in files:
             return os.path.join(root, name)
-    
+
+
 def search(name, path):
     search_res = []
     for root, dirs, files in os.walk(path):
         for file in files:
-            if file.count(name)+file.count('.txt') > 1:
-                search_res.append(file.removesuffix('.txt'))
+            if file.count(name) + file.count(".txt") > 1:
+                search_res.append(file.removesuffix(".txt"))
     return search_res
 
 
@@ -27,33 +29,34 @@ def full_input(input_line):
             lines.append(line)
         else:
             break
-    return '\n'.join(lines)
+    return "\n".join(lines)
 
 
 while True:
-    os.system('cls')
+    os.system("cls")
 
-    name = input('enter the file name\n')
-    if name == 'E':
+    name = input("enter the file name\n")
+    if name == "E":
         break
 
     file_path = find(name, DIR_PATH)
     while file_path == None:
-        print('file not found please retry')
+        print("file not found please retry")
         search_res = search(name, DIR_PATH)
-        if len(search_res)==1:
-            print('found this.. ' + search_res[0])
+        if len(search_res) == 1:
+            print("found this.. " + search_res[0])
             file_path = find(search_res[0], DIR_PATH)
         else:
             print(search_res)
             name = input()
             file_path = find(name, DIR_PATH)
-        
 
-    print('file path is.. ' + file_path + '\n')
+    print("file path is.. " + file_path + "\n")
 
-    description = full_input('\nenter the desctiption you want')
+    description = full_input("\nenter the desctiption you want")
 
-    with open(file_path, 'w') as file_object:
+    # currecting frequent spelling mistakes
+    description = description.replace("Theraputic", "Therapeutic")
+
+    with open(file_path, "w") as file_object:
         file_object.write(description)
-
